7c850e26678477c6d0e582b67de9a0e35a0429ba
AleAgnelli92 <aleagnelli92@gmail.com>
2024-02-26 15:25:13 +0100

    Add project file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var prod1 = document.getElementById('prod1');
+var prod2 = document.getElementById('prod2');
+var prod3 = document.getElementById('prod3');
+var prod4 = document.getElementById('prod4');
+var prod5 = document.getElementById('prod5');
+var prod6 = document.getElementById('prod6');
+var prod7 = document.getElementById('prod7');
+var prod8 = document.getElementById('prod8');
+
+function apriDettaglio() {
+    // window.location.href = "personale";
+}
